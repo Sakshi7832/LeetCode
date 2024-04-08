4549348ccfb5adcd5f93d791c05ca85606beb40f
Sakshi <84897436+Sakshi7832@users.noreply.github.com>
2024-04-09 01:17:22 +0530

    Time: 1 ms (84.36%), Space: 40.6 MB (79.90%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Solution {
+    public static int reverse(int x) {
+        long rev = 0;
+
+       
+            while(x>0 || x<0)
+            {   
+                int rem = x%10;
+                rev = rev*10+rem;
+                x = x/10;
+
+                if(rev>Integer.MAX_VALUE || rev<Integer.MIN_VALUE)
+                {
+                    return 0;
+                }
+
+}
+            
+             return (int) rev;
+            
+
+        }
+        
+    
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(reverse(n));
+    }
+}
